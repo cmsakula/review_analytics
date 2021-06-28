9b5d63f30da5b0e6bc23e6ee02bd60c2a1f6f7ed
Eric Tang <etang619@gmail.com>
2021-06-28 17:24:24 +0800

    get average length

--- review_analytics.py
+++ review_analytics.py
@@ -1,12 +1,19 @@
 
 data =[]
 count = 0
+message_length = 0
 with open('reviews.txt', 'r') as f:
 	for line in f:
-		count += 1
 		data.append(line.strip()) # strip remove white space and \n 
-		if count % 1000 == 0:
-			print(len(data))
+		message_length = len(data[count]) + message_length
+		count += 1
+		#if count % 1000 == 0:
+		#	print(len(data))
 
-print(len(data))
-print(data[0])
+print('there are ', len(data), 'messages. ', 'The average message length is ', message_length/len(data))
+print('the message_length is ', message_length)
+
+message_length = 0
+for d in data:
+	message_length = message_length + len(d)
+print('the next message_length us ', message_length)
