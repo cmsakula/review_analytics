44da9b7b3359fd33e767747a6c27f74d72488b0e
Eric Tang <etang619@gmail.com>
2021-06-28 17:14:51 +0800

    print out progress

--- review_analytics.py
+++ review_analytics.py
@@ -1,8 +1,12 @@
 
 data =[]
+count = 0
 with open('reviews.txt', 'r') as f:
 	for line in f:
+		count += 1
 		data.append(line.strip()) # strip remove white space and \n 
-		#print(line)
+		if count % 1000 == 0:
+			print(len(data))
+
 print(len(data))
 print(data[0])
